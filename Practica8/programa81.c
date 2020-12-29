7c6fc40ac3a4b0f397a98883c6ea11ab9c1a2c6d
AntonioArce <tony.ipn26@gmail.com>
2020-12-29 17:49:56 -0600

    archivo para el programa81

---
+++ Practica8/programa81.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/type.h>
+#include<dirent.h>
+
+void menuprincipal()
+{
+	puts("*************Menu Principal**************");
+	puts("1. Crear Archivo");
+	puts("2. Modificar Archivo");
+	puts("3. Borrar Archivo");
+	puts("4. Salir");
+}
+
+int main()
+{
+	
+	return 0;
+}
